71e5e454841ec3e533bd5e5ff9928525af542cc5
Ladislav Smola <lsmola@redhat.com>
2019-02-25 17:40:26 +0100

    Parse network info and send mac_addresses to ingress

--- lib/amazon/parser/vm.rb
+++ lib/amazon/parser/vm.rb
@@ -7,11 +7,12 @@ module Amazon
         flavor = lazy_find(:flavors, :source_ref => instance.instance_type) if instance.instance_type
 
         vm = TopologicalInventoryIngressApiClient::Vm.new(
-          :source_ref  => uid,
-          :uid_ems     => uid,
-          :name        => name,
-          :power_state => parse_vm_power_state(instance.state),
-          :flavor      => flavor,
+          :source_ref    => uid,
+          :uid_ems       => uid,
+          :name          => name,
+          :power_state   => parse_vm_power_state(instance.state),
+          :flavor        => flavor,
+          :mac_addresses => parse_network(instance)[:mac_addresses],
         )
 
         collections[:vms].data << vm
@@ -21,6 +22,27 @@ module Amazon
       end
 
       private
+
+      def parse_network(instance)
+        network = {
+          :fqdn                 => instance.public_dns_name,
+          :private_ip_address   => instance.private_ip_address,
+          :public_ip_address    => instance.public_ip_address,
+          :mac_addresses        => [],
+          :private_ip_addresses => [],
+          :public_ip_addresses  => [],
+        }
+
+        (instance&.network_interfaces || []).each do |interface|
+          network[:mac_addresses] << interface.mac_address
+          interface.private_ip_addresses.each do |private_ip|
+            network[:private_ip_addresses] << private_ip.private_ip_address
+            network[:public_ip_addresses] << private_ip&.association&.public_ip if private_ip&.association&.public_ip
+          end
+        end
+
+        network
+      end
 
       def parse_vm_tags(vm_uid, tags)
         tags.each do |tag|
