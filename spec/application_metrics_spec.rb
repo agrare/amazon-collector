17d2b9f23bc8e9ed59e1c406cd6ff5e46550682f
Martin Slemr <mslemr@redhat.com>
2019-05-22 12:36:48 +0200

    Metrics port as an argument

--- spec/application_metrics_spec.rb
+++ spec/application_metrics_spec.rb
@@ -6,22 +6,33 @@ RSpec.describe TopologicalInventory::Amazon::Collector::ApplicationMetrics do
   subject! { described_class.new(9394) }
   after    { subject.stop_server }
 
-  it "exposes metrics" do
-    subject.record_error
-    subject.record_error
+  context "Turned on" do
+    it "exposes metrics" do
+      subject.record_error
+      subject.record_error
 
-    metrics = get_metrics
-    expect(metrics["topological_inventory_amazon_collector_errors_total"]).to eq("2")
-  end
-
-  def get_metrics
-    metrics = Net::HTTP.get(URI("http://localhost:9394/metrics")).split("\n").delete_if do |e|
-      e.blank? || e.start_with?("#")
+      metrics = get_metrics
+      expect(metrics["topological_inventory_amazon_collector_errors_total"]).to eq("2")
     end
 
-    metrics.each_with_object({}) do |m, hash|
-      k, v = m.split
-      hash[k] = v
+    def get_metrics
+      metrics = Net::HTTP.get(URI("http://localhost:9394/metrics")).split("\n").delete_if do |e|
+        e.blank? || e.start_with?("#")
+      end
+
+      metrics.each_with_object({}) do |m, hash|
+        k, v = m.split
+        hash[k] = v
+      end
+    end
+  end
+
+  context "Turned off" do
+    subject! { described_class.new(0) }
+
+    it "doesn't raise exception" do
+      expect { subject.record_error }.not_to raise_exception
+      expect { subject.stop_server }.not_to raise_exception
     end
   end
 end
