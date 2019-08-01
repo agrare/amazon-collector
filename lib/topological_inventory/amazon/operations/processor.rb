94736d18905e04e355afe9e99116499ba632080b
Alberto Bellotti <abellott@redhat.com>
2019-08-01 13:57:38 -0400

    Adding hook in Processor to invoke the model.method to handling the inbound messages.

--- lib/topological_inventory/amazon/operations/processor.rb
+++ lib/topological_inventory/amazon/operations/processor.rb
@@ -12,15 +12,29 @@ module TopologicalInventory
 
         def initialize(message)
           self.message = message
+          self.model, self.method = message.message.split(".")
+
+          self.params   = message.payload["params"]
+          self.identity = message.payload["request_context"]
         end
 
         def process
-          # TODO: handle the operation
+          logger.info("Processing #{model}##{method} [#{params}]...")
+
+          impl = Operations.const_get(model).new(params, identity) if Operations.const_defined?(model)
+          unless impl
+            logger.error("#{model}.#{method} is not implemented")
+            return
+          end
+
+          result = impl.send(method)
+          logger.info("Processing #{model}##{method} [#{params}]...Complete")
+          result
         end
 
         private
 
-        attr_accessor :message
+        attr_accessor :message, :identity, :model, :method, :params
       end
     end
   end
